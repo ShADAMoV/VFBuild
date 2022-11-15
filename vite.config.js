2467f38b3aefa176655d32b478ba2ba6d552e20b
Валентин <admin@creator-spb.ru>
2022-11-15 14:55:10 +0300

    update settings

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
+import {fileURLToPath, URL} from "node:url";
+
 const fs = require('fs')
 const path = require('path')
 import { defineConfig } from 'vite'
 
-const PAGES = fs.readdirSync('src/pages').filter((filename) => filename !== 'index.html');
+const PAGES = fs.readdirSync('./pages');
 
 const getInputs = () => {
     return PAGES.reduce((inputs, page) => {
-        inputs[page.replace('.html', '')] = path.resolve(__dirname, `./src/pages/${page}`);
+        inputs[page.replace('.html', '')] = path.resolve(__dirname, `./pages/${page}`);
 
         return inputs;
     }, {});
@@ -16,10 +18,15 @@ const INPUTS = getInputs();
 
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
     build: {
         rollupOptions: {
             input: {
-                home: path.resolve(__dirname, 'src/pages/index.html'),
+                home: path.resolve(__dirname, 'index.html'),
                 ...INPUTS
             },
         }
